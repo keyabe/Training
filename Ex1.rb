cada6703737a4ee2f8693a8eb2b336cc5b21b94d
keyabe <keyabe99@gmail.con>
2022-01-11 09:19:02 +0200

    first commit

---
+++ Ex1.rb
@@ -0,0 +1,7 @@
+puts "Скажите своё имя"
+name = gets.chomp
+puts "Скажите своё отчество"
+patr = gets.chomp
+puts "Скажите свою фамилию"
+family = gets.chomp
+puts "Приветствую! " + family + " " + name + " " + patr
